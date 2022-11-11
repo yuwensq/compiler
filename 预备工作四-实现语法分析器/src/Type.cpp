42d3fbcf2b01d6beaa7a52f710c8168b9d9d63f7
孙昊鹏 <791442388@qq.com>
2022-11-11 20:13:38 +0800

    Update Type.cpp

--- 预备工作四-实现语法分析器/src/Type.cpp
+++ 预备工作四-实现语法分析器/src/Type.cpp
@@ -4,28 +4,44 @@
 #include <sstream>
 
 IntType TypeSystem::commonInt = IntType(4);
+FloatType TypeSystem::commonFloat = FloatType(4);
 IntType TypeSystem::commonConstInt = IntType(4, true);
+FloatType TypeSystem::commonConstFloat = FloatType(4, true);
 VoidType TypeSystem::commonVoid = VoidType();
 
-Type* TypeSystem::intType = &commonInt;
-Type* TypeSystem::constIntType = &commonConstInt;
-Type* TypeSystem::voidType = &commonVoid;
+Type *TypeSystem::intType = &commonInt;
+Type *TypeSystem::floatType = &commonFloat;
+Type *TypeSystem::constIntType = &commonConstInt;
+Type *TypeSystem::constFloatType = &commonConstFloat;
+Type *TypeSystem::voidType = &commonVoid;
 
-std::string IntType::toStr() {
+std::string IntType::toStr()
+{
     if (constant)
         return "const int";
     else
         return "int";
 }
 
-std::string VoidType::toStr() {
+std::string FloatType::toStr()
+{
+    if (constant)
+        return "const float";
+    else
+        return "float";
+}
+
+std::string VoidType::toStr()
+{
     return "void";
 }
 
-std::string FunctionType::toStr() {
+std::string FunctionType::toStr()
+{
     std::ostringstream buffer;
     buffer << returnType->toStr() << "(";
-    for (auto it = paramsType.begin(); it != paramsType.end(); it++) {
+    for (auto it = paramsType.begin(); it != paramsType.end(); it++)
+    {
         buffer << (*it)->toStr();
         if (it + 1 != paramsType.end())
             buffer << ", ";
@@ -34,7 +50,8 @@ std::string FunctionType::toStr() {
     return buffer.str();
 }
 
-std::string StringType::toStr() {
+std::string StringType::toStr()
+{
     std::ostringstream buffer;
     buffer << "const char[" << length << "]";
     return buffer.str();
